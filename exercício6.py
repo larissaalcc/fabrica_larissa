d66777430905420d5e9f79b995424aa7bc758b79
Gabriel Gravena Barros <132004882+larissaalcc@users.noreply.github.com>
2023-05-12 17:08:25 -0300

    exercício5,6

---
+++ exercício6.py
@@ -0,0 +1,14 @@
+a= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print (sum(a))/len(a)
+
+uma_lista= ['a', 'b', 'c', 'd', 'e', 'f']
+print(uma_lista[1:3])
+print(uma_lista[:4])
+print(uma_lista[3:])
+print(uma_lista[:])
+print(uma_lista[0:6])
+
+frutas= ["banana", "maçã", "cereja"]
+frutas[0]= "pera"
+frutas [-1]= "laranja"
+print(frutas)
