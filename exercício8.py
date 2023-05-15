bf3cb2fa91fbea469809b55ee6fdce11c893196c
Gabriel Gravena Barros <larissacgr05@gmail.com>
2023-05-15 18:31:13 -0300

    8

---
+++ exercício8.py
@@ -0,0 +1,10 @@
+l= str(input("Seu sexo: Digite 'f' PARA FEMININO, 'm' PARA MASCULINO OU 'o' PARA OUTROS: "))
+if l== 'f': 
+    print("Feminino")
+elif l== 'm': 
+    print ("Masculino")
+elif l =='o':
+    print("Outro")
+else:
+    print("Sexo inválido")
+
