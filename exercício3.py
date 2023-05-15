8d5a115d9ecd11e87ccccb731360ab64bc79dd41
Gabriel Gravena Barros <larissacgr05@gmail.com>
2023-05-15 16:58:16 -0300

    3,4

---
+++ exercício3.py
@@ -0,0 +1,15 @@
+idade =  int(input("Qual sua idade? "))
+if idade == 16: 
+    print('Você pode votar!')
+else: 
+    if idade>=16:
+        print("você pode dirigir!")
+    else: 
+        if idade<16:
+            print('Apenas estude!')
+
+
+
+
+   
+
