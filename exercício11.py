a6823ae18a41069b7caaf85f5f5c41d4bee5a868
Gabriel Gravena Barros <larissacgr05@gmail.com>
2023-05-17 17:52:18 -0300

    dia 17

--- exercício11.py
+++
@@ -1,9 +0,0 @@
-n1= int(input("Digite um número: "))
-n2= int(input("Digite um segundo número: "))
-n3= int(input("Digite um terceiro número: "))
-if n1>n2 and n2>n3:
-    print(n1, n3)
-if n2>n1 and n3>n1:
-    print(n2, n1)
-elif n3>n1 and n2>n1:
-    print(n3, n2)
--- exercício12.py
+++ exercício12.py
@@ -1,9 +1,12 @@
+n1= str(input("Nome do produto 1:"))
 p1= float(input('preço 1: '))
+n2= str(input("Nome do produto 2:"))
 p2= float(input('preço 2: '))
+n3= str(input("Nome do produto 3:"))
 p3= float(input('preço 3: '))
 if p1<p2 and p1<p3:
-    print (p1)
-if p2>p1 and p2<p3:
-    print (p2)
+    print ("O produto %s é mais barato e custa R$%s"%(n1,p2))
+if p2<p1 and p2<p3:
+    print ("O produto %s é mais barato e custa R$%s" %(n2,p2))
 if p3<p1 and p3<p2:
-    print (p3)
+    print ("O produto %s é mais barato e custa R$%s"% (n3,p3))
