2521619bc91738380b6f77d778491cc9a7249965
Gabriel Gravena Barros <larissacgr05@gmail.com>
2023-05-24 14:43:45 -0300

    0

--- maurício.py
+++ maurício.py
@@ -5,6 +5,6 @@ area= (3.14*r*r)
 print("A área do círculo é igual a ",(area))
 print('------OBRIGADO------')
 
-
+#um programa que leia números do usuário e conte quantos números pares e quantos números ímpares foram digitados.
 
 
