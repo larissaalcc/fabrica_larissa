c3338cb665c4a348ea1d86947019082492a83e73
Gabriel Gravena Barros <larissacgr05@gmail.com>
2023-05-26 14:39:40 -0300

    Update zemarretateste.py

--- zemarretateste.py
+++ zemarretateste.py
@@ -1,5 +1,1 @@
-
-print(" >>>>>> Bem vindo a divisão<<<<<<")
-num1=float(input(" Digite um numero:"))
-num2=float(input(" Digite outro numero:"))
-print(" >>>>>>Obrigado por dividir com a gente!<<<<<<")
+n1= in
